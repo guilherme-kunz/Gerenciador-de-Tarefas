540d9b13ece12085abb28e9bb868038fd46da1ba
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2025-02-27 20:55:19 -0300

    feat: TaskDao.kt

---
+++ app/src/main/java/com/guilhermekunz/gerenciadordetarefas/TaskDao.kt
@@ -0,0 +1,27 @@
+package com.guilhermekunz.gerenciadordetarefas
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("SELECT * FROM tasks WHERE isSynced = 0")
+    suspend fun getUnsyncedTasks(): List<Task>
+}
