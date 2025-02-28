1a40e20c018f6688d7c4da869d8336eecaee3726
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2025-02-28 11:25:35 -0300

    feat: create TaskViewModel (TaskViewModel.kt)

---
+++ app/src/main/java/com/guilhermekunz/gerenciadordetarefas/TaskViewModel.kt
@@ -0,0 +1,33 @@
+package com.guilhermekunz.gerenciadordetarefas
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class TaskViewModel(private val repository: TaskRepository) : ViewModel() {
+
+    val tasks: StateFlow<List<Task>> = repository.tasks.stateIn(
+        viewModelScope, SharingStarted.Lazily, emptyList()
+    )
+
+    fun addTask(task: Task) {
+        viewModelScope.launch {
+            repository.addTask(task)
+        }
+    }
+
+    fun updateTask(task: Task) {
+        viewModelScope.launch {
+            repository.updateTask(task)
+        }
+    }
+
+    fun deleteTask(task: Task) {
+        viewModelScope.launch {
+            repository.deleteTask(task)
+        }
+    }
+}
