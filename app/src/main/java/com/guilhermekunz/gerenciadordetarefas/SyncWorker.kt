8b6556ed6893b9db8039e58c60cefc8adf0bc1f7
Guilherme Butzen Kunz <guilhermebutzenkunz@gmail.com>
2025-02-28 11:34:54 -0300

    feat: create SyncWorker.kt

---
+++ app/src/main/java/com/guilhermekunz/gerenciadordetarefas/SyncWorker.kt
@@ -0,0 +1,17 @@
+package com.guilhermekunz.gerenciadordetarefas
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+
+class SyncWorker(
+    context: Context,
+    workerParams: WorkerParameters,
+    private val repository: TaskRepository
+) : CoroutineWorker(context, workerParams) {
+
+    override suspend fun doWork(): Result {
+        repository.syncTasks()
+        return Result.success()
+    }
+}
